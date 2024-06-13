e22d463f4a10ca7e7eac648312649e6a5c897b7d
Minsung <alscks6521@naver.com>
2024-06-13 12:01:21 +0900

    드래그 앤 드랍 위젯 Rx, Stateful 각 사용

--- lib/main/widgets/pick_card.dart
+++ lib/main/widgets/pick_card.dart
@@ -75,3 +75,76 @@ class PickCard extends StatelessWidget {
     );
   }
 }
+
+
+// ## Stateful 사용한 드래그 제스처
+
+// import 'package:flutter/material.dart';
+
+// class PickCard extends StatefulWidget {
+//   const PickCard({super.key});
+
+//   @override
+//   State<PickCard> createState() => _PickCardState();
+// }
+
+// class _PickCardState extends State<PickCard> {
+//   Widget _buildCard(Color color) {
+//     Widget child(isFeedBack) => Container(
+//           width: 100,
+//           height: 100,
+//           decoration: BoxDecoration(
+//             color: color,
+//             borderRadius: BorderRadius.circular(4),
+//             border: Border.all(width: isFeedBack ? 3 : 0),
+//           ),
+//         );
+
+//     return Draggable(
+//       data: color,
+//       feedback: child(true),
+//       child: child(false),
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(18.0),
+//       child: Column(
+//         children: [
+//           Expanded(
+//             child: DragTarget(
+//               onAcceptWithDetails: (details) {
+//                 debugPrint("${details.data}");
+//               },
+//               builder: (context, candidateData, rejectedData) {
+//                 String text = 'Drage And Drop';
+//                 if (candidateData.isNotEmpty) {
+//                   text = 'Drop the Card!';
+//                 }
+//                 return Container(
+//                   decoration: BoxDecoration(
+//                     border: Border.all(),
+//                     borderRadius: BorderRadius.circular(10),
+//                   ),
+//                   alignment: Alignment.center,
+//                   child: Text(text),
+//                 );
+//               },
+//             ),
+//           ),
+//           Expanded(
+//               child: Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceAround,
+//             children: [
+//               _buildCard(Colors.red),
+//               _buildCard(Colors.blue),
+//               _buildCard(Colors.yellow),
+//             ],
+//           ))
+//         ],
+//       ),
+//     );
+//   }
+// }
