50444aa8ff1729d23a0e53bf70faaa8655546b9c
Minsung <alscks6521@naver.com>
2024-06-13 12:00:27 +0900

    Drage And Drop

--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:daelim_univ/common/widgets/app_scaffold.dart';
 import 'package:daelim_univ/localization/app_string.dart';
+import 'package:daelim_univ/main/widgets/pick_card.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,12 +10,37 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
-        drawerEnableOpenDragGesture: true,
-        appBar: AppBar(
-          title: Text(
-            AppString.home.tr,
-          ),
+      drawerEnableOpenDragGesture: true,
+      appBar: AppBar(
+        title: Text(
+          AppString.home.tr,
         ),
-        child: const Placeholder());
+      ),
+      child: const DefaultTabController(
+        length: 2,
+        child: Column(
+          children: [
+            TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.directions_car),
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_transit),
+                ),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  PickCard(),
+                  Text('Tab2'),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
